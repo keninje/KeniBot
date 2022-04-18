76d3062df29ca527a3404fbf47d57255af6f2230
keninje <keninje@gmail.com>
2022-04-18 19:06:09 +0300

    make the commands search recursive and move the songs commands to their own folder

--- common.js
+++ common.js
@@ -1,16 +1,25 @@
-import fs from 'node:fs';
+import fsp from 'node:fs/promises';
 
-const getCommands = async (dir) => {
-    const commands =
-        fs.readdirSync(dir)
-            .filter(file => file.endsWith('.js'))
+const getModulesFromFiles = (files) =>
+    Promise.all(
+        files.filter(file => file.endsWith('.js'))
             .map(async file => {
-                const { default: command } = await import(`${dir}/${file}`)
+                const { default: command } = await import(`${file}`)
                 console.log(`Loaded ${file}`)
                 return command
-            });
-    return Promise.all(commands);
+            })
+    );
+
+const getFilesRecursively = async (dir) => {
+    const files = await fsp.readdir(dir, { withFileTypes: true })
+    const commandsFiles = await Promise.all(files.map(file => {
+        const path = `${dir}/${file.name}`
+        return file.isDirectory() ? getFilesRecursively(path) : path;
+    }));
+    return Array.prototype.concat(...commandsFiles)  
 }
+
+const getCommands = async (dir) => getModulesFromFiles(await getFilesRecursively(dir));
 
 const getCommandsMap = async (dir) => (await getCommands(dir)).reduce((map, command) => {
     const { data: { name } } = command
