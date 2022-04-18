2538878eb13ce8da84489bf514026c1bb8aae478
keninje <keninje@gmail.com>
2022-04-19 02:35:23 +0300

    remove unnecessary logging

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,10 @@ import { Client, Intents } from "discord.js";
 import Command from "./types/command";
 import { getCommandsMap } from "./common.js";
 import { token } from './config.js'
-import path from "path";
 
 class KeniClient extends Client<boolean> {
     commands: Map<string, Command>;
-    
+
     constructor(commands: Map<string, Command>) {
         super({ intents: [Intents.FLAGS.GUILDS] })
         this.commands = commands;
@@ -20,9 +19,7 @@ client.once('ready', () => {
 })
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return ;
-
-    console.log(interaction)
+    if (!interaction.isCommand()) return;
 
     const command = client.commands.get(interaction.commandName)
 
