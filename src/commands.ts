66b799c5a6e0956f8c17eabb9883dca191ad1b62
keninje <keninje@gmail.com>
2022-04-19 15:05:28 +0300

    Remove sensitive info and use env vars

--- src/commands.ts
+++ src/commands.ts
@@ -1,24 +1,26 @@
 import { REST } from "@discordjs/rest"
 import { Routes } from "discord-api-types/v10"
-import { clientId, guildId, token } from "./config.js"
+import { clientId, token } from "./config.js"
 import { getCommands } from "./common.js"
 
 const commands = (await getCommands('./commands')).map(c => c.data.toJSON())
 
-const rest = new REST({version: '10'}).setToken(token)
+const rest = new REST({ version: '10' }).setToken(token)
 
-const addCommandsToGuild = () => {
+const addCommandsToGuild = (guildId: string) => {
     rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-        .then(() => console.log("Successfully registered app commands!"))
+        .then(() => console.log("Successfully registered app commands to the guild!"))
         .catch(console.error)
 }
 
 const addCommandsGlobally = () => {
-    
+    rest.put(Routes.applicationCommands(clientId), { body: commands })
+    .then(() => console.log("Successfully registered app commands globally!"))
+    .catch(console.error)
 }
 
 if (process.argv.includes('--global')) {
     addCommandsGlobally()
 } else {
-    addCommandsToGuild()
+    addCommandsToGuild(process.argv.at(2)!!)
 }
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config()
+export const
+    clientId = process.env.CLIENT_ID!!,
+    token = process.env.DISCORD_TOKEN!!
