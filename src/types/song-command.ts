5e15f0e332723f7fc695fa462736f396c3b851a9
keninje <keninje@gmail.com>
2022-04-20 12:35:38 +0300

    verify roles on song commands

--- src/types/song-command.ts
+++ src/types/song-command.ts
@@ -1,4 +1,4 @@
-import { CacheType, CommandInteraction, GuildMember, VoiceBasedChannel } from "discord.js";
+import { CacheType, CommandInteraction, GuildMember, Permissions, VoiceBasedChannel } from "discord.js";
 import Command from "./command";
 import CustomClient from "./custom-client";
 import SlashCommand from "./slash-command";
@@ -9,17 +9,30 @@ abstract class SongCommand implements Command {
     async execute(interaction: CommandInteraction<CacheType>, client: CustomClient): Promise<void> {
         const voiceChannel = this.getVoiceChannel(interaction)
         if (voiceChannel === null) {
-            await interaction.reply({ content: "You have to be in a voice channel to execute commands!" })
-        } else {
+            await interaction.reply("You have to be in a voice channel to execute commands!")
+        } else if (await this.checkBotPermissions(interaction, voiceChannel)) {
             await this.executeSongCommand(interaction, voiceChannel, client);
         }
     }
 
     abstract executeSongCommand(interaction: CommandInteraction<CacheType>, voiceBasedChannel: VoiceBasedChannel, client: CustomClient): Promise<void>
 
-    getVoiceChannel(interaction: CommandInteraction<CacheType>): VoiceBasedChannel | null {
+    private getVoiceChannel(interaction: CommandInteraction<CacheType>): VoiceBasedChannel | null {
         const guildMember = interaction.member as GuildMember;
         return guildMember.voice.channel;
+    }
+
+    private async checkBotPermissions(interaction: CommandInteraction<CacheType>, voiceChannel: VoiceBasedChannel): Promise<boolean> {
+        const permissions = voiceChannel.permissionsFor(interaction.client.user!!)!!
+        if (!permissions.has(Permissions.FLAGS.CONNECT)) {
+            await interaction.reply("I do not have the permission to connect to the Voice Channel")
+            return false;
+        }
+        else if (!permissions.has(Permissions.FLAGS.SPEAK)) {
+            await interaction.reply("I do not have the permission to speak in the Voice Channel")
+            return false
+        }
+        return true;
     }
 }
 
